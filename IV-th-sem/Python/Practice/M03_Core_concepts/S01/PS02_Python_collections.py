f2a6e225fa34ee74de4400650e1acc39a65c0835
pavangoud69 <pavangoudkankati@gmail.com>
2026-03-09 12:08:14 +0530

    Assignments

---
+++ IV-th-sem/Python/Practice/M03_Core_concepts/S01/PS02_Python_collections.py
@@ -0,0 +1,28 @@
+'''t = (1,2,3,4,5)
+t[0]= 10
+print(t[0])
+'''
+#git commands
+'''
+dir -Force
+git init
+git add .
+git commit -m "Assignments"
+git pull origin main
+git push origin main
+git status
+'''
+
+'''t = (1,2,3,4,5)
+t2 = (2,4,6,8)
+print(t + t2)
+print(t[1:])
+print(t[0:3])
+'''
+d = {"name":"pavan",'age':20}
+print(d)
+del d['age']
+print(d.pop('name'))
+print(d.popitem())
+d.clear()
+print(d)
