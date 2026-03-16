98628548faccb5c4aeda40b5e61c0aa63213ea2a
pavangoud69 <pavangoudkankati@gmail.com>
2026-03-16 11:14:28 +0530

    commit

---
+++ IV-th-sem/Python/Practice/M03_Core_concepts/S03/PS03_Built_in_Functions.py
@@ -0,0 +1,15 @@
+'''
+1)Find the largest number (using(max))
+2)Check Palindrome (Using reversed() & join())
+3)Count Even Numbers (Using(filter()))
+4)Remove Duplicates( Using sum())
+5)Sum of DIgits (using(sum()))
+6) sort words alphabetically (using(sor()))
+7)find common elements(using(set()))
+8)index with calue (using Enumerate())
+9)pair two lists(using zip())
+10)find second largest number(using sorted())'''
+a = [10,20,87,15,98,12,3,2,45,100]
+print(max(a))
+
+s = int
--- IV-th-sem/Python/assignments/M01_Language_and_Programming_Basics/AST03/task.py
+++ IV-th-sem/Python/assignments/M01_Language_and_Programming_Basics/AST03/task.py
@@ -1,12 +1,17 @@
-def calculate_result(name: str, grades: list) -> str:
-    #task 3
-    average = sum(grades) / len(grades)
-    status = "Pass" if average >= 40 else "Fail"
-    return f"Average grade: {average:.2f}, Status: {status}"
+def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
+    avg = (n1 + n2 + n3) / 3
+
+    if avg >= 50:
+        status = "Pass"
+        avg = round(avg, 1)
+    else:
+        status = "fail"
+        avg = int(avg * 100) / 100
+
+    return f"Average grade: {avg}, Status: {status}"
 
 
-if __name__ == "__main__":
-    name = input().strip()
-    grades = list(map(int, input().split()))
-    
-    print(calculate_result(name, grades))
+if __name__ == '__main__':
+    name = input()
+    n1, n2, n3 = list(map(int, input().split()))
+    print(Student_Grade_System(name, n1, n2, n3))
